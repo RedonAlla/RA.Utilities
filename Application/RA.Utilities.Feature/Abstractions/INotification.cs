f901a2ebdc8676f94f6a55af6c351593e65aec2a
Redon Alla <39186570+RedonAlla@users.noreply.github.com>
2025-10-25 20:00:39 +0200

    Delete Application directory

--- Application/RA.Utilities.Feature/Abstractions/INotification.cs
+++
@@ -1,6 +0,0 @@
-namespace RA.Utilities.Feature.Abstractions;
-
-/// <summary>
-/// Represents a notification that can be published and handled by zero or more handlers.
-/// </summary>
-public interface INotification { }
--- Application/RA.Utilities.Feature/Models/NotificationHandlerDelegate.cs
+++
@@ -1,8 +0,0 @@
-using System.Threading.Tasks;
-
-namespace RA.Utilities.Feature.Models;
-
-/// <summary>
-/// Represents a delegate for handling notifications.
-/// </summary>
-public delegate Task NotificationHandlerDelegate();
