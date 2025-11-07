e5bb86eb8a2401c7fd1e2b0a60bef9c35a3afe21
Redon Alla <redon.alla@gmail.com>
2025-11-07 19:46:48 +0100

    upgrade home page banner

--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -7,12 +7,27 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
+import Illustration from '../../static/img/BannerIllustration.svg';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={styles.heroBanner}>
       <div className="container">
+        <div className="row">
+          <div className="col col--8">
+          <Heading as="h3" className="hero__title">
+            {siteConfig.title}
+          </Heading>
+            <p>{siteConfig.tagline}</p>
+          </div>
+          <div className="col col--4">
+            <Illustration width={'25rem'} height={'25rem'} />
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
@@ -24,7 +39,7 @@ function HomepageHeader() {
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
-      </div>
+      </div> */}
     </header>
   );
 }
@@ -34,7 +49,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={siteConfig.title}
-      description="Description will go into a meta tag in <head />">
+      description='RA.Utilities is a "batteries-included" framework for building modern .NET APIs. It provides the foundation for a clean architecture so you can focus more on writing business logic and less on setting up infrastructure.'>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
