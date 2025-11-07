a96bedb38cfb3ec56643024f30bfbf7394fbac28
Redon Alla <redon.alla@gmail.com>
2025-11-08 00:29:19 +0100

    feat(docs): enhance intro and style changes for homepage
    Added a detailed section on the purpose and benefits of RA.Utilities NuGet Packages to the introduction document, providing clearer guidance for developers. Updated HomepageFeatures component to improve styling and interface by switching from CSS modules to SCSS for more flexible styling, including standardized iconography and improved layout with SCSS. Removed redundant SVG imports and switched to using simpler icon representation, enhancing overall maintainability and readability of the codebase.

--- documentation/src/components/HomepageFeatures/index.tsx
+++ documentation/src/components/HomepageFeatures/index.tsx
@@ -1,71 +1,65 @@
 import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import './styles.scss';
+import Link from '@docusaurus/Link';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: ReactNode;
+  icon: string;
+  description: string;
+  link: string;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    title: 'NuGet',
+    icon: 'nuget',
+    link: '/nuget-packages/intro',
+    description: 'NuGet packages designed to accelerate modern .NET API development. These packages provide reusable, production-ready building blocks to solve common problems, allowing you to focus on business logic instead of boilerplate code. Easily integrate into your projects via NuGet for fast setup and consistent behavior.',
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+    title: 'Project Template',
+    icon: 'visual_studio',
+    link: 'vs-template/intro',
+    description: "A powerful foundation for building applications using ASP.NET Core and Clean Architecture principles. It promotes a structured, maintainable, and scalable development workflow by separating concerns and enforcing clear boundaries. Designed to reduce complexity and improve long-term performance, it empowers teams to deliver robust solutions with confidence and efficiency. Whether you're launching a new product or modernizing legacy systems, this template gives you the confidence and clarity to build software that lasts."
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    title: 'Item Templates',
+    icon: 'template',
+    link: '/nuget-packages/intro',
+    description: "RA.Utilities includes a curated set of item templates that supercharge your development workflow. These templates provide ready-to-use scaffolding for common components—like Endpoints, Features, and DTOs—so you can focus on writing meaningful code, not boilerplate.  RA.Utilities item templates help you move faster with confidence."
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, icon, link, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
+      <div className={clsx('single-services', icon, 'text--center')} >
+          <div className="services-icon align-items-center justify-content-center">
+            <i />
+          </div>
+          <div className="services-content">
+            <h4 className="services-title">
+              <Link to={link}>{title}</Link>
+            </h4>
+            <p className="text"> { description }</p>
+          </div>
+        </div>
     </div>
   );
 }
 
 export default function HomepageFeatures(): ReactNode {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+    <section className='services-area'>
+        <div className="container">
+          <div className="row">
+            {FeatureList.map((props, idx) => (
+              <Feature key={idx} {...props} />
+            ))}
+          </div>
         </div>
-      </div>
     </section>
   );
 }
